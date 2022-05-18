9fc6c8bfd36792352d4182b3f84ffc8607948f3a
Temitope Jegede <temiteejegede@gmail.com>
2022-05-18 16:11:30 -0400

    refractoring code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middlewear/not-found");
+
 require("dotenv").config();
 
 //middleware
@@ -10,6 +12,8 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
