510cfabb608b69bb82b08b628575d525cbb3a1d1
Temitope Jegede <temiteejegede@gmail.com>
2022-05-18 14:53:59 -0400

    updating api routes

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+
 //middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
-//routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager app");
-});
 
+//routes
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
