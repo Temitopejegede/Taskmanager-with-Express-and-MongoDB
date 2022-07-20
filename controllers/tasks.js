1e21a60cb6242caccf9f4c98ff3a2845b4611a9a
temitopejegede <temiteejegede@gmail.com>
2022-07-20 18:44:24 -0400

    added comments

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,26 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middlewear/async");
 const { createCustomError } = require("../errors/custom-error");
+
+/**
+ * get all tasks from database
+ */
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
 });
 
+/**
+ * create a new task
+ */
 const createTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body);
   res.status(201).json({ task });
 });
 
+/**
+ * get a task using its id
+ */
 const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
@@ -20,6 +30,9 @@ const getTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
+/**
+ * update a task
+ */
 const updateTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
@@ -32,6 +45,9 @@ const updateTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
+/**
+ * dete a task using its id
+ */
 const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
